7e460bd16c5940fcc93f5932464b29d75700a4e8
nachohotz <juanhotz611@gmail.com>
2022-01-07 14:50:12 -0300

    finish Home.tsx

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,4 +1,5 @@
 import CommitItem from '../components/CommitItem';
+import style from './Home.module.css';
 
 type HomeProps = {
   commits: Array<any>;
@@ -7,10 +8,33 @@ type HomeProps = {
 export default function Home({ commits }: HomeProps) {
   return (
     <div>
-      <h2>fulltimeforce-challenge commits history</h2>
-      {commits.map((commit) => {
-        return <CommitItem commit={commit} />;
-      })}
+      <header className={style.header}>
+        <h2>fulltimeforce-challenge commits history</h2>
+        <h4>
+          By{' '}
+          <a
+            href="https://github.com/NachoHotz/fulltimeforce-challenge"
+            target="_blank"
+            rel="noopener noreferer"
+          >
+            Nacho Hotz
+          </a>
+        </h4>
+      </header>
+      <main className={style.mainContainer}>
+        {commits.map((commit) => {
+          return (
+            <CommitItem
+              key={commit.sha}
+              sha={commit.sha}
+              authorName={commit.commit.author.name}
+              authorEmail={commit.commit.author.email}
+              date={commit.commit.author.date}
+              message={commit.commit.message}
+            />
+          );
+        })}
+      </main>
     </div>
   );
 }
