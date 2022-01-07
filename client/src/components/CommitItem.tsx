26b231412cee5928b5f5d57f64870566182426fe
nachohotz <juanhotz611@gmail.com>
2022-01-07 15:51:45 -0300

    finish CommitItem

--- client/src/components/CommitItem.tsx
+++ client/src/components/CommitItem.tsx
@@ -1,11 +1,34 @@
+import style from './CommitItem.module.css';
+
 type CommitItemProps = {
-  commit: object;
+  sha: string;
+  authorName: string;
+  authorEmail: string;
+  date: string;
+  message: string;
 };
 
-export default function CommitItem({ commit }: CommitItemProps) {
+export default function CommitItem({
+  sha,
+  authorName,
+  authorEmail,
+  date,
+  message,
+}: CommitItemProps) {
+  const dateFormatted = new Date(date);
   return (
-    <div>
-      <h3>commit</h3>
-    </div>
+    <article className={style.commitContainer}>
+      <section className={style.miscContainer}>
+        <h4>{sha}</h4>
+        <h4>{`${dateFormatted.getDay()}/${
+          dateFormatted.getMonth() + 1
+        }/${dateFormatted.getFullYear()} - ${dateFormatted.getHours()}:${dateFormatted.getMinutes()}:${dateFormatted.getSeconds()}`}</h4>
+      </section>
+      <section className={style.authorInfo}>
+        <p>{authorName}</p>
+        <p> - ({authorEmail})</p>
+      </section>
+      <p className={style.commitMessage}>{message}</p>
+    </article>
   );
 }
