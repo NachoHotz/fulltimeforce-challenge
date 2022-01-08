896e1adff37478ea8f8c6ccfd10eb2e2f90010c7
nachohotz <juanhotz611@gmail.com>
2022-01-08 11:42:21 -0300

    add frontend url for deployment

--- api/src/middlewares/setHeaders.ts
+++ api/src/middlewares/setHeaders.ts
@@ -5,7 +5,7 @@ const setHeaders = (_req: Request, res: Response, next: NextFunction) => {
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
+    'https://fulltimeforce-challenge-odr381fi9-nachohotz.vercel.app/',
   );
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
