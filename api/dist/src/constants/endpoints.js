990442e3a5422a607c7b6b3dea4f1c484753ad1d
Nacho Hotz <juanhotz611@gmail.com>
2022-01-08 10:41:57 -0300

    Delete api/dist directory

--- api/dist/src/constants/endpoints.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.endpoints = void 0;
-exports.endpoints = {
-    API_URL: 'https://api.github.com/repos/nachohotz/fulltimeforce-challenge/commits',
-};
--- api/dist/src/services/basicService.service.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var basicService = function () { return ({
-    success: true,
-    code: 200,
-    message: 'This route is for testing. Head to /github to get the actual information',
-}); };
-exports.default = basicService;
