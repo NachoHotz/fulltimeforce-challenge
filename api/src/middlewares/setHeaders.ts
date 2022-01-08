755e70becd9cb5c1d3ad6749141067c18589f871
nachohotz <juanhotz611@gmail.com>
2022-01-08 11:47:13 -0300

    change frontend url

--- api/src/middlewares/setHeaders.ts
+++ api/src/middlewares/setHeaders.ts
@@ -1,11 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 
 const setHeaders = (_req: Request, res: Response, next: NextFunction) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://fulltimeforce-challenge.vercel.app/',
+  );
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header(
     'Access-Control-Allow-Headers',
-    'https://fulltimeforce-challenge-odr381fi9-nachohotz.vercel.app/',
+    'Origin, X-Requested-With, Content-Type, Accept',
   );
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
