03038017d1298a27b36b08dc72b00383a5078ff4
nachohotz <juanhotz611@gmail.com>
2022-01-08 11:36:20 -0300

    add heroku url

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,9 @@ function App() {
 
   const fetchCommits = async () => {
     try {
-      const { data } = await axios.get('http://localhost:3001/github');
+      const { data } = await axios.get(
+        'https://fulltimeforce-challenge-api.herokuapp.com/github',
+      );
       setCommits(data);
     } catch (e) {
       console.error(e);
