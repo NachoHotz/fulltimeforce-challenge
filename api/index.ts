8705a1b2522f46026e9be150c949016b2b207036
nachohotz <juanhotz611@gmail.com>
2022-01-08 10:28:50 -0300

    add files for prod

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,8 @@
 import app from './src/app';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const API_PORT: number = 3001;
+const API_PORT: number | string = process.env.PORT || 3001;
 
 app.listen(API_PORT, () => {
   console.log(`server running on port ${API_PORT}`);
