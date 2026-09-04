a29bedf206c2010b9063935161a65ba440ec0763
Nacho Hotz <juanhotz611@gmail.com>
2026-09-04 16:58:37 -0300

    Remove template literals for API_URL in getCommits

--- client/src/handler/getInfo.ts
+++ client/src/handler/getInfo.ts
@@ -6,5 +6,5 @@ const { NODE_ENV, REACT_APP_SERVER_DEV_URL, REACT_APP_SERVER_PROD_URL } = proces
 const API_URL = NODE_ENV === 'development' ? REACT_APP_SERVER_DEV_URL : REACT_APP_SERVER_PROD_URL
 
 export async function getCommits() {
-  return await axios.get<CommitInfo[]>(`${API_URL}`)
+  return await axios.get<CommitInfo[]>(API_URL)
 }
